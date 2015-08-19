1d6aeab3d92e24ad0c1332d27a91329f03824bf2
janmilosh <janmilosh@gmail.com>
2015-08-19 19:10:06 -0400

    Adds service and test files

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -3,39 +3,12 @@
 var drumControllers = angular.module('drumControllers', []);
 
 drumControllers.controller('HomeController', function ($scope, Song) {
-  var drumlist = [
-                    ['Clap', 'clap.mp3'],
-                    ['Clave', 'clave.mp3'],
-                    ['Cow Bell', 'cow-bell.mp3'],
-                    ['Crash', 'crash-1.mp3'],
-                    ['Hi Hat', 'hi-hat-1.mp3'],
-                    ['Kick Drum', 'kick-drum-1.mp3'],
-                    ['Maracas', 'maracas.mp3'],
-                    ['Rim Shot', 'rim-shot.mp3'],
-                    ['Snare Drum', 'snare-drum-1.mp3']
-                  ]
 
-  function Song(name) {
-    this.name = name;
-    this.drums = {}
-  }
-
-  function addDrumlistToSong(song, drumlist) {
-    song.drums = []
-    angular.forEach(drumlist, function(drum) {
-      song.drums.push({
-                        title: drum[0],
-                        link: 'sounds/' + drum[1],
-                        beatsArray: []
-                      });
-    });
-  }
-
-  var testSong = new Song('testsong')
+  var testSong = new Song('testSong');
 
   console.log(testSong.name);
-
-  addDrumlistToSong(testSong, drumlist);
   console.log(testSong.drums);
+
+
 
 });
--- scripts/services.js
+++ scripts/services.js
@@ -1,7 +1,35 @@
 'use strict';
 
-var drumServices = angular.module('drumServices', ['ngResource']);
+var drumServices = angular.module('drumServices', []);
 
-drumServices.factory('Song', function(){
-  
+drumServices.service('Song', function(){  
+  var drumlist = [
+                    ['Clap', 'clap.mp3'],
+                    ['Clave', 'clave.mp3'],
+                    ['Cow Bell', 'cow-bell.mp3'],
+                    ['Crash', 'crash-1.mp3'],
+                    ['Hi Hat', 'hi-hat-1.mp3'],
+                    ['Kick Drum', 'kick-drum-1.mp3'],
+                    ['Maracas', 'maracas.mp3'],
+                    ['Rim Shot', 'rim-shot.mp3'],
+                    ['Snare Drum', 'snare-drum-1.mp3']
+                  ]
+
+  var populateDrumlist = function(drumArray) {
+    angular.forEach(drumlist, function(drum) {
+      drumArray.push({
+                  title: drum[0],
+                  link: 'sounds/' + drum[1],
+                  beatsArray: []
+                });
+    });
+  }
+
+  var Song = function(name) {
+    this.name = name;
+    this.drums = []
+    populateDrumlist(this.drums);
+  }
+
+  return Song;
 });
